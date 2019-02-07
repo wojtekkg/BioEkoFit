9766039690a2c31f4b519c4ab4da04f603b2effb
wojtekg <wojtekg@hotmail.com>
2019-02-07 20:10:34 +0100

    Added new projects

---
+++ Src/bio-eko-fit-menus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bio_eko_fit_menus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
